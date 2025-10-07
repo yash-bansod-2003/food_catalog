2dfa00351a1a4ee9b1b5c4ca4433f504b8390e92
yashbansod <yashbansodgit@gmail.com>
2025-10-08 01:11:19 +0530

    feat: implement message broker factory for Kafka integration

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,16 @@ import { createServer } from "@/server.js";
 import configuration from "@/config/configuration.js";
 import logger from "@/config/logger.js";
 import { connectDatabase } from "@/config/connect-database.js";
-import { KafkaBroker } from "@/common/services/kafka.js";
+import { createMessageBrokerFactory } from "./common/factories/brokerFactory.js";
 
 const port = configuration.port ? parseInt(configuration.port) : 5000;
 const host = configuration.host ?? "localhost";
 const server: Application = createServer();
-const kafkaMessageBroker = new KafkaBroker({
-  clientId: configuration.kafka.clientId,
-  brokers: configuration.kafka.brokers,
-});
+const messageBroker = createMessageBrokerFactory();
 server.listen(port, host, async () => {
   try {
     await connectDatabase();
-    await kafkaMessageBroker.connect();
+    await messageBroker.connect();
     logger.info("Kafka connected successfully");
     logger.info(`Server Listening on  http://${host}:${port}`);
   } catch (error: unknown) {
